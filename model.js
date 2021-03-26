b0da68c1f088ea3965eaa0f687f2022da80e0c28
Vlados-Lavandos <81446503+Vlados-Lavandos@users.noreply.github.com>
2021-03-27 00:16:00 +0200

    Add files via upload

---
+++ model.js
@@ -0,0 +1,58 @@
+module.exports = {
+    "student@ukma.edu.ua": {
+        "fullName": "Student Student ",
+        "faculty": "faculty",
+        "specialty": "specialty",
+        "year": 2,
+        "semesters": [
+            {
+                "semester": 3,
+
+
+                "courses": [
+                    {
+                        "name": "Course AAA",
+                        "lecturer": "Full Name",
+                        "mark": 101,
+                        "topics": [
+                            {
+                                "title": "AA"
+                            },
+                            {
+                                "title": "BB"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "Course BB",
+                        "lecturer": "Full name",
+                        "mark": 69,
+                        "topics": [
+                            {
+                                "title": "BA",
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "semester": 4,
+                "courses": [
+                    {
+                        "name": "Course 2B",
+                        "lecturer": "Full name",
+                        "mark": 99,
+                        "topics": [
+                            {
+                                "title": "AA1",
+                            },
+                            {
+                                "title": "AA2",
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
